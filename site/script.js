fa002c3617e20a412917babba26b242868f1a774
viocio <152957773+viocio@users.noreply.github.com>
2026-01-03 21:06:05 +0200

    Delete site directory

--- site/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("btn");
-  const msg = document.getElementById("message");
-
-  btn.addEventListener("click", () => {
-    msg.textContent = "JavaScript loaded and executed!";
-  });
-});
